66f53009445998a991836bcdee23cafb035ec3a0
saied2035 <saied2421998@gmail.com>
2023-10-21 12:36:10 +0300

    Finish user authentication logic and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  include JwtToken
+  before_action :authenticate_request
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { error: exception.message }, status: 400
+  end
+
+  private
+
+  def authenticate_request
+    token = request.cookies['token']
+    user_id = JwtToken.decode(token)['user_id'].to_i
+    @current_user = User.find(user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-    has_secure_password
+  has_secure_password
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, confirmation: true
-    validates :password_confirmation, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_confirmation, presence: true
 end
