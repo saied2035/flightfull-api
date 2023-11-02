da95b70f775e9103d75dab89cccaa8f10480dd26
saied2035 <saied2421998@gmail.com>
2023-11-02 15:24:03 +0200

    Add create/delete airlne logic

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -1,8 +1,27 @@
 class AirlinesController < ApplicationController
     
     def create
+      airline = @current_user.airlines.build(airline_params)
+      if airline.save
+        airlines = Airline.all
+        user_airlines = @current_user.airlines
+        render json: { airlines: airlines, user_airlines: user_airlines }, status: :created
+      else
+        render json: { error: airline.errors.full_messages }, status: 400
+      end    
     end
     
-    def destory
-    end    
+    def destroy
+      @current_user.airlines.destroy(Airline.find(params[:id]))
+      airlines = Airline.all
+      user_airlines = @current_user.airlines
+      render json: { airlines: airlines, user_airlines: user_airlines }, status: :ok
+    end
+    
+    private
+
+    def airline_params
+        params.require(:airline).permit(:name, :description, :img_src, :fee, :option_to_purchase, 
+        :total_amount_payable, :facebook, :twitter, :linkedin)
+    end
 end
