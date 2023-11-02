84652ca8ac2bd10d8a1b01bdbc2fa6cc2ef60359
saied2035 <saied2421998@gmail.com>
2023-11-02 14:42:19 +0200

    Add relationship between user&airline model/enhance validations in airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,7 +1,9 @@
 class Airline < ApplicationRecord
+    belongs_to :user, optional: true
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true
-    validates :img_src, presence: true, format: { with: /(^https|base64)/, message: "image src should be base64 string or start with image valid url" }
+    validates :img_src, presence: true, format: { with: /(^https|base64)/, 
+    message: "image src should be base64 string or start with image valid url" }
     validates :fee, presence: true, numericality: true
     validates :option_to_purchase, presence: true, numericality: true
     validates :total_amount_payable, presence: true, numericality: true
@@ -14,4 +16,6 @@ class Airline < ApplicationRecord
     validates :linkedin, presence: true, format: { 
     with: /\Ahttps\:\/\/(www.linkedin|linkedin)\.com\/[^?=*&^]+\z/ 
     }
+
+    default_scope { order(created_at: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :airlines, -> { order('created_at DESC') }
   has_secure_password
 
   validates :name, presence: true
